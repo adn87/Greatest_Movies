977ed8a5e747081ec04fb6fb9d49a2659c3e95a1
adn87 <aaghadieh@gmail.com>
2023-11-22 08:03:01 -0500

    Greatest_Movies_001
    - using python to web scrape the empire website to make a txt file that contains the top 100 Greatest movies in order (1-100).
    - using requests library to get access to the website.
    - creating a new txt file using open file syntax.
    - using list comprehension to add the name of the movies to the text file.

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+response = requests.get(URL)
+webpage = response.text
+
+soup = BeautifulSoup(webpage, "html.parser")
+
+
+movie_names = soup.find_all(name="h3", class_="title")
+movie_titles = [movie.getText() for movie in movie_names]
+
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w", encoding="utf8") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
+
+
+
